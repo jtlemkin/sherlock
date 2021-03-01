e90311a556f7e861d1d6bd659f773e2bae027906
jtlemkin <jtlemkin@gmail.com>
2021-02-28 18:46:01 -0800

    added error catching to getContracts

--- server/api.ts
+++ server/api.ts
@@ -4,13 +4,17 @@ const etherscanAPIKey = "P5J1B9HADZM199WWRBR1X31T72E98PDRCF"
 const etherscanBaseURL = "https://api.etherscan.io/api?module=contract&action=getsourcecode"
 
 export async function getContracts(contractID: string) {
-    const url = etherscanBaseURL + "&address=" + contractID + "&apikey=" + etherscanAPIKey
-    console.log("URL", url)
-    const response = await axios.get(url)
-    const brokenSourceCodeJSONString = response.data["result"][0]["SourceCode"]
-    const fixedSourceCodeObject = JSON.parse(brokenSourceCodeJSONString.slice(1, brokenSourceCodeJSONString.length - 1))
-    const contracts = (Object.values(fixedSourceCodeObject["sources"]) as any[])
-        .map(contract => contract["content"])
+    try {
+        const url = etherscanBaseURL + "&address=" + contractID + "&apikey=" + etherscanAPIKey
+        console.log("URL", url)
+        const response = await axios.get(url)
+        const brokenSourceCodeJSONString = response.data["result"][0]["SourceCode"]
+        const fixedSourceCodeObject = JSON.parse(brokenSourceCodeJSONString.slice(1, brokenSourceCodeJSONString.length - 1))
+        const contracts = (Object.values(fixedSourceCodeObject["sources"]) as any[])
+            .map(contract => contract["content"])
 
-    return contracts
+        return contracts
+    } catch(err) {
+        throw err
+    }
 }
