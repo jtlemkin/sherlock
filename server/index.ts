5670bfca91d3bc9ef480ffe30f1395f8560b6268
jtlemkin <jtlemkin@gmail.com>
2021-02-28 18:45:02 -0800

    added ability to download etherscan contracts

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,21 @@
 import express from 'express'
+import { getContracts } from './api'
 
 const app = express()
 const PORT = 8000
 
 app.get('/', (req, res) => res.send("Hello, World!"))
+
+app.get('/contract/:contractID', async (req, res) => {
+    try {
+        const result = await getContracts(req.params["contractID"])
+        res.json(result)
+    } catch(err) {
+        console.log("ERROR", err)
+        res.send(err)
+    }
+})
+
 app.listen(PORT, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`)
 })
